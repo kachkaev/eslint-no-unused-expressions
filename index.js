e922c7b575016f445f949786d1db90c64fd2345a
Alexander Kachkaev <alexander.kachkaev@woodsbagot.com>
2016-11-21 19:00:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import injectGlobal from 'styled-components';
+
+// OK (with brackets)
+injectGlobal(`
+  body {
+    color: #000,
+  }
+`);
+
+// causes eslint error (no brackets)
+injectGlobal`
+  body {
+    background: #f00,
+  }
+`;
